bee8270bc6da499ec1157307ad0efdf47a2dd1f8
saadtariq-ds <tariqsaad1997@gmail.com>
2025-12-21 02:31:35 +0500

    Creating Vector Store

--- config/config.py
+++ config/config.py
@@ -5,3 +5,7 @@ load_dotenv()
 HUGGINGFACE_API_KEY = os.getenv("HUGGINGFACE_API_KEY")
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 LLM_MODEL_NAME = "llama-3.1-8b-instant"
+EMBEDDING_MODEL_NAME = "sentence-transformers/all-MiniLM-L6-v2"
+
+CHUNK_SIZE = 1000
+CHUNK_OVERLAP = 0
---
+++ src/vector_store.py
@@ -0,0 +1,65 @@
+"""
+This modules handle the Chroma DB Vector Store functionality.
+"""
+
+import os
+from langchain_community.document_loaders import CSVLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import Chroma
+from langchain_huggingface import HuggingFaceEmbeddings
+from config.config import (
+    HUGGINGFACE_API_KEY, EMBEDDING_MODEL_NAME, 
+    CHUNK_SIZE, CHUNK_OVERLAP
+)
+
+
+class AnimeVectorStore:
+    def __init__(self, csv_file_path: str, presist_directory: str = "chroma_db"):
+        self.csv_file_path = csv_file_path
+        self.presist_directory = presist_directory
+        self.embedding = HuggingFaceEmbeddings(
+            model_name=EMBEDDING_MODEL_NAME,
+            huggingfacehub_api_token=HUGGINGFACE_API_KEY
+        )
+
+    def create_vector_store(self) -> Chroma:
+        """
+        Create and persist a Chroma vector store from the CSV data.
+        """
+
+        # Load data from CSV
+        loader = CSVLoader(
+            file_path=self.csv_file_path, 
+            encoding='utf-8',
+            metadata_columns=[]
+        )
+        documents = loader.load()
+
+        # Split text into smaller chunks
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=CHUNK_SIZE,
+            chunk_overlap=CHUNK_OVERLAP,
+            separators=["\n\n", "\n", " ", ""]
+        )
+        split_documents = text_splitter.split_documents(documents=documents)
+
+        # Create Chroma vector store
+        vector_store = Chroma.from_documents(
+            documents=split_documents,
+            embedding=self.embedding,
+            persist_directory=self.presist_directory
+        )
+
+        # Persist the vector store to disk
+        vector_store.persist()
+
+        return vector_store
+    
+    def load_vector_store(self) -> Chroma:
+        """ Load an existing Chroma vector store from disk."""
+        # Load existing Chroma vector store from disk
+        vector_store = Chroma(
+            embedding_function=self.embedding,
+            persist_directory=self.presist_directory
+        )
+        return vector_store
